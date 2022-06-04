ddc59ee9ba3ec112cd501467b8ad1d9accd6710e
huydt03 <87981977+huydt03@users.noreply.github.com>
2022-06-04 11:03:06 +0700

    Update RoomsBase.js

--- src/RoomsBase.js
+++ src/RoomsBase.js
@@ -1,83 +1,136 @@
-const InfoBase = require('@drhuy/infobase');
+const _SOCKET_KEYS = {
+	PLAYER: {
+		UPDATE: 'player.update',
+		OTHER_UPDATE: 'player.other_update',
+	},
+	ROOMS: {
+		GET_PLAYERS: 'rooms.get_players',
+		GET_ROOMS: 'rooms.get_rooms',
+		ENTER: 'room.enter',
+		LEAVE: 'room.leave',
+	},
+	ROOM: {
+		GET_PLAYERS: 'room.get_players',
+		CREATE: 'room.create',
+		DESTROY: 'room.destroy',
+		ENTER: 'room.enter',
+		LEAVE: 'room.leave',
+		UPDATE: 'room.update',
+		KICKPLAYER: 'room.kickplayer'
+	},
+	ERROR: 'error',
+	DISCONNECT: 'disconnect'
+}
 
-function RoomsBase(id, info){
+function SocketAppBase(io, RoomPanel, Socket_keys) {
 
-	const EVENTS = [
-		'onBeforePlayerEnter',
-		'onAfterPlayerEnter',
-		'onBeforePlayerLeave',
-		'onAfterPlayerLeave',
-		'onBeforeAddRoom',
-		'onAfterAddRoom',
-		'onBeforeDestroyRoom',
-		'onAfterDestroyRoom',
-		'onPlayerEntered',
-	];
+	let self = RoomPanel;
 
-	let players = {};
+	let SOCKET_KEYS = {..._SOCKET_KEYS, ...Socket_keys};
 
-	let rooms = {};
+	function getSocketById(id){
+		let socket = io.sockets.sockets.get(id);
+		return socket;
+	}
 
-	let self = new InfoBase(id, info);
+	self.onAfterPlayerEnter = function(_player){
+		let player = _player[0];
 
-	// rooms
-	self.addRoom = function(room){
-		let id = room.id;
-		self.handle.onBeforeAddRoom.fire(room);
-		room.onAfterPlayerLeave = function(){
-			if(!room.nPlayers)
-				self.destroyRoom(room);
+		function _onPlayerUpdateInfo(){
+			if(!player.roomPanelId)
+				player.handle.onUpdateInfo.remove(_onPlayerUpdateInfo);
+			if(player.roomId)
+				io.to(player.roomId).emit(SOCKET_KEYS.PLAYER.OTHER_UPDATE, player.getInfo());
+			io.to(self.id).emit(SOCKET_KEYS.PLAYER.OTHER_UPDATE, player.getInfo());
 		}
-		rooms[id] = room;
-		self.handle.onAfterAddRoom.fire(room);
+		player.onUpdateInfo = _onPlayerUpdateInfo;
+		player.socketId = player._socketId;
+		player.online = true;
 
-		return room;
+		let socketId = player.socketId;
+		let socket = getSocketById(socketId);
+		if(socket){
+			socket.join(self.id);
+			if(room = self.getRoom(player.roomId))
+				room.playerEnter(player)
+			else{
+				socket.emit(SOCKET_KEYS.ROOMS.GET_ROOMS, self.getRooms());
+				socket.emit(SOCKET_KEYS.ROOMS.GET_PLAYERS, self.getPlayers());
+			}
+		}
+		io.to(self.id).emit(SOCKET_KEYS.ROOMS.ENTER, player.getInfo())
 	}
 
-	self.destroyRoom = function(room){
-		let id = room.id;
-		self.handle.onBeforeDestroyRoom.fire(room);
-		delete rooms[id];
-		self.handle.onAfterDestroyRoom.fire(room);
+	self.onAfterPlayerLeave = function(player){
+		player = player[0];
+		
+		io.to(self.id).emit(SOCKET_KEYS.ROOMS.LEAVE, player.id)
+		
+		let socketId = player.socketId;
+		let socket = getSocketById(socketId);
+		if(socket){
+			socket.leave(self.id);
+		}
 	}
 
-	self.playerEnter = function(player){
-		self.handle.onBeforePlayerEnter.fire(player);
-		let id = player.id;
-		if(players[id])
-			self.handle.onPlayerEntered.fire(players[id]);
-		players[id] = player;
-		player.roomPanelId = self.id;
-		self.handle.onAfterPlayerEnter.fire(player);
+	self.onPlayerEntered = function(player){
+		player = player[0];
+		let socketId = player.socketId;
+		let socket = getSocketById(socketId);
+		if(socket){
+			socket.emit(SOCKET_KEYS.OTHER_CONNECT);
+			socket.disconnect();
+		}
 	}
 
-	self.playerLeave = function(playerId){
-		let player = players[playerId];
-		if(!player)
-			return;
-		self.handle.onBeforePlayerLeave.fire(player);
-		player.roomPanelId = null;
-		let roomId = player.roomId;
-		let room = rooms[roomId];
-		if(room)
-			room.playerLeave(playerId);
-		delete players[playerId];
-		self.handle.onAfterPlayerLeave.fire(player);
+	self.onAfterAddRoom = function(_room){
+		let room = _room[0];
+
+		io.to(self.id).emit(SOCKET_KEYS.ROOM.CREATE, room.getInfo())
+		
+		room.onAfterPlayerEnter = function(player){
+			player = player[0];
+
+			io.to(room.id).emit(SOCKET_KEYS.ROOM.ENTER, player.getInfo())
+
+			let socketId = player.socketId;
+			let socket = getSocketById(socketId);
+			if(socket){
+				socket.join(room.id);
+				socket.leave(self.id);
+				socket.emit(SOCKET_KEYS.ROOM.GET_PLAYERS, room.getPlayers());
+			}
+		}
+
+		room.onUpdateInfo = function(){
+			let info = room.getInfo();
+			io.to(self.id).emit(SOCKET_KEYS.ROOM.UPDATE, info);
+			io.to(room.id).emit(SOCKET_KEYS.ROOM.UPDATE, info);
+		}
+
+		room.onAfterPlayerLeave = function(player){
+			player = player[0];
+
+			io.to(room.id).emit(SOCKET_KEYS.ROOM.LEAVE, player.id)
+
+			let socketId = player.socketId;
+			let socket = getSocketById(socketId);
+			if(socket){
+				socket.join(self.id);
+				socket.leave(room.id);
+				socket.emit(SOCKET_KEYS.ROOMS.GET_PLAYERS, self.getPlayers())
+				socket.emit(SOCKET_KEYS.ROOMS.GET_ROOMS, self.getRooms())
+			}
+		}
+
 	}
 
-	function init(){
-		self.handle.add(EVENTS);
-		self.players = players;
-		self.rooms = rooms;
 
-		Object.defineProperties(self, {
-			players: { get: function(){ return players } },
-			rooms: { get: function(){ return rooms } }
-		});
+	self.onAfterDestroyRoom = function(room){
+		io.to(self.id).emit(SOCKET_KEYS.ROOM.DESTROY, room[0].id)
 	}
-	init();
 
 	return self;
 }
 
-module.exports = RoomsBase;
+module.exports = SocketAppBase;
